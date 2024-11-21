dde196fbcba7a91940a3cee9618a0b24eaa47e36
Guilherme Filheiro <guilherme.filheiro@vortigo.com.br>
2024-11-21 16:55:06 -0300

    autalização grande

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-from app.api import appointment, user
-from .db import database, engine, Base
+from api import appointment, user
+from fastapi.middleware.cors import CORSMiddleware
+from db import database, engine, Base
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",  # Endereço do seu cliente React em desenvolvimento
+    "http://127.0.0.1:3000",  # Alternativa para o cliente local
+    # Adicione outras origens permitidas se necessário
+]
 
 Base.metadata.create_all(bind=engine) 
 
@@ -11,6 +18,15 @@ app = FastAPI()
 # Registro das rotas
 app.include_router(appointment.router)
 app.include_router(user.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
--- app/services/appointment.py
+++ app/services/appointment.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
-from app.models.appointment import Appointment
-from app.schemas.appointment import AppointmentCreate
+from models.appointment import Appointment
+from schemas.appointment import AppointmentCreate, AppointmentUpdateDTO
 
 def create(db: Session, appointment: AppointmentCreate):
     db_appointment = Appointment(**appointment.model_dump())
+    db_appointment.status = "não concluído"
+    db_appointment.active = True
     db.add(db_appointment)
     db.commit()
     db.refresh(db_appointment)
@@ -16,5 +18,26 @@ def list(db: Session):
     return db.query(Appointment).all()
 
 def delete(db: Session, appointment_id: int):
-    return db.query(Appointment).filter(Appointment.id == appointment_id).delete()
+    db.query(Appointment).filter(Appointment.id == appointment_id).delete()
+    return db.commit()
+
+def update(db: Session, appointment_id: int, appointment_update: AppointmentUpdateDTO):
+    db_appointment = db.query(Appointment).filter(Appointment.id == appointment_id).first()
+    
+    if db_appointment:
+        # Atualiza os campos do agendamento com os valores fornecidos no DTO
+        if appointment_update.description is not None:
+            db_appointment.description = appointment_update.description
+        if appointment_update.date is not None:
+            db_appointment.date = appointment_update.date
+        if appointment_update.status is not None:
+            db_appointment.status = appointment_update.status
+        if appointment_update.active is not None:
+            db_appointment.active = appointment_update.active
+        
+        db.commit()
+        db.refresh(db_appointment)
+        return db_appointment
+    
+    return None
 
